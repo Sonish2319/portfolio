112b67521308622fcc25d6566777690681df9304
Sonish2319 <83855936+Sonish2319@users.noreply.github.com>
2026-09-10 09:45:05 +0545

    portfolio content update based on current CV v2

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -255,7 +255,7 @@ export default function Hero() {
           onMouseEnter={() => setHovered(true)}
           onMouseLeave={() => setHovered(false)}
         >
-          {/* Default: "I am / Sonish / Upadhyaya" */}
+          
           <span
             className="jp-default-text"
             style={{
@@ -271,7 +271,7 @@ export default function Hero() {
             <em style={{ fontStyle: "italic", color: "#A78BFA" }}>Sonish</em>
             <br />
             <span style={{ WebkitTextStroke: "1px rgba(123,140,222,0.3)", color: "transparent" }}>
-              Upadhyaya upadhyaya Jenkin pipeline testing
+              Upadhyaya
             </span>
           </span>
 
